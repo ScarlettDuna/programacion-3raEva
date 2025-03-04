17664462befc12f2ede8a988bca8edab997fc3e9
ScarlettDuna <arantxa.rg.1991@hotmail.com>
2025-03-04 21:01:34 +0100

    deberes desde casa, lo que faltaba

--- src/CrearXMLfile.java
+++ src/CrearXMLfile.java
@@ -1,5 +1,6 @@
 import org.w3c.dom.*;
 import javax.xml.transform.*;
+import javax.xml.transform.Transformer;
 import javax.xml.transform.dom.*;
 import javax.xml.transform.stream.*;
 import javax.xml.parsers.*;
--- src/Transformer.java
+++ src/Transformer.java
@@ -1,2 +1,29 @@
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
+import java.io.*;
+
 public class Transformer {
+    public static void main(String[] args) throws IOException {
+        String hojaEstilo = "alumnosPlantilla.xsl";
+        String datosAlumnos = "alumnos.xml";
+        File pagHTML = new File("mipagina.html");
+
+        // crear fichero HTML
+        FileOutputStream os = new FileOutputStream(pagHTML);
+        Source estilos = new StreamSource(hojaEstilo);
+        Source datos = new StreamSource(datosAlumnos);
+
+        // resultado transformacion
+        Result result = new StreamResult(os);
+        try {
+            javax.xml.transform.Transformer transformer = TransformerFactory.newInstance().newTransformer(estilos);
+            transformer.transform(datos, result);
+        } catch (Exception e) {
+            System.err.println("Error: " + e);
+        }
+        os.close();
+    }
 }
