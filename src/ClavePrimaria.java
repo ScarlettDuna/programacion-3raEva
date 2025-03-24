94207ff57e537b6c956d0bb3d41b321b63edfb78
ScarlettDuna <arantxa.rg.1991@hotmail.com>
2025-03-24 21:29:24 +0100

    ejercicio 2.10

--- src/ClavePrimaria.java
+++ src/ClavePrimaria.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.sql.*;
 
 public class ClavePrimaria {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Establecer la conexión con la BD
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/ejemplo?serverTimezone=Europe/Madrid", "anchanDB", "AnchanDB2025");
+            DatabaseMetaData dbmd = conexion.getMetaData();
+            ResultSet pk = dbmd.getPrimaryKeys(null, "ejemplo", "departamentos");
+            String pkDep = "", separador="";
+            while (pk.next()) {
+                pkDep = pkDep + separador + pk.getString("COLUMN_NAME");
+                separador = "+";
+            }
+            System.out.println("Clave primaria: " + pkDep);
+        } catch (ClassNotFoundException |SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/TablaClavePrimaria.java
+++ src/TablaClavePrimaria.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.sql.*;
 
 public class TablaClavePrimaria {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Establecer la conexión con la BD
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/ejemplo?serverTimezone=Europe/Madrid", "anchanDB", "AnchanDB2025");
+            DatabaseMetaData dbmd = conexion.getMetaData();
+            ResultSet fk = dbmd.getExportedKeys(null, "ejemplo", "departamentos");
+            while (fk.next()) {
+                String fk_name = fk.getString("FKCOLUMN_NAME");
+                String pk_name = fk.getString("PKCOLUMN_NAME");
+                String fk_tablename = fk.getString("FKTABLE_NAME");
+                String pk_tablename = fk.getString("PKTABLE_NAME");
+                System.out.printf("Table PK: %s, Clave Primaria: %s %n", pk_tablename, pk_name);
+                System.out.printf("Table FK: %s, Clave Ajena: %s %n", fk_tablename, fk_name);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
